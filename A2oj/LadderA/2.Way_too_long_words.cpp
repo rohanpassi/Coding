a32aa8f58c1573ecc456f913fd24d61c0a777e80
Rohan Passi <rohanpassi94@gmail.com>
2018-10-19 14:32:20 -0700

    some practice

---
+++ A2oj/LadderA/2.Way_too_long_words.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define SZ 1234567
+#define MOD7 10000007
+#define MOD9 1000000009
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(x) x.resize(unique(ALL(x)) - x.begin());
+#define max3(a, b, c) max(a, max(b, c))
+#define min3(a, b, c) min(a, min(b, c))
+#define max4(a, b, c, d) max(max(a, b), max(c, d))
+#define min4(a, b, c, d) min(min(a, b), min(c, d))
+#define loop(i, a, b) for(ll i=a; i<b; i++)
+#define loopr(i, a, b) for(ll i=a; i>=b; i--)
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	ll n;
+	cin>>n;
+	
+	while(n--){
+		string s;
+		cin>>s;
+		ll len = s.length();
+		if(len <= 10){
+			cout<<s<<endl;
+		}
+		else{
+			cout<<s[0]<<len-2<<s[len-1]<<endl;
+		}
+	}	
+	
+	return 0;
+}
